6daf9ca976867fdd970265465b1b3ea5cbed316d
Cryptomaker <cryptomaker@gmail.com>
2013-09-08 22:06:44 +0800

    Update checkpoints.cpp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -31,6 +31,8 @@ namespace Checkpoints
         (         2495, uint256("0xb51275730e1423faee36b7b4a8c7f240fbbffa240bcc173b16eb6fddbd4cc39a"))
         (         3249, uint256("0xc4a239118335a2773631c701d063102476593261d17ffd4739f09f9d43d878d0"))
         (         4016, uint256("0xfa3eca3e310e7da62f460456d3f30f994b60963f3014104fbb2b617443880633"))
+        (         22531, uint256("0x5eeac09c8749be8a94306579405a655277e6ae5daf888bba569f876751083399"))
+        (         22546, uint256("0x319e31accd10075e70f4f473b685d28276ec1c12047c843302b7e1f103f7848b"))
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
